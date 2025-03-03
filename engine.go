6bb70144189658ab067efa576c1d1f1a35258d86
ZhaoChen <ittisennsinn@gmail.com>
2025-03-03 21:56:51 +0800

    feat: support transaction

--- engine.go
+++ engine.go
@@ -12,6 +12,8 @@ type Engine struct {
 	dialect dialect.Dialect
 }
 
+type TxFunc func(*session.Session) (interface{}, error)
+
 func NewEngine(driver, source string) (e *Engine, err error) {
 	db, err := sql.Open(driver, source)
 	if err != nil {
@@ -35,14 +37,35 @@ func NewEngine(driver, source string) (e *Engine, err error) {
 	return
 }
 
-func (e *Engine) Close() {
-	err := e.db.Close()
+// Transaction 将所有的操作放到一个回调函数中，作为入参传递给 engine.Transaction()
+// 发生任何错误，自动回滚，如果没有错误发生，则提交
+func (engine *Engine) Transaction(f TxFunc) (result interface{}, err error) {
+	s := engine.NewSession()
+	if err := s.Begin(); err != nil {
+		return nil, err
+	}
+	defer func() {
+		if p := recover(); p != nil {
+			_ = s.Rollback()
+			panic(p) // re-throw panic after Rollback
+		} else if err != nil {
+			_ = s.Rollback() // err is non-nil; don't change it
+		} else {
+			err = s.Commit() // err is nil; if Commit returns error update err
+		}
+	}()
+
+	return f(s)
+}
+
+func (engine *Engine) Close() {
+	err := engine.db.Close()
 	if err != nil {
 		log.Error(err)
 	}
 	log.Info("Close database success")
 }
 
-func (e *Engine) NewSession() *session.Session {
-	return session.NewSession(e.db, e.dialect)
+func (engine *Engine) NewSession() *session.Session {
+	return session.NewSession(engine.db, engine.dialect)
 }
--- engine_test.go
+++ engine_test.go
@@ -1,10 +1,17 @@
 package engine
 
 import (
+	"errors"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
+	"go-orm/session"
 	"testing"
 )
+
+type User struct {
+	Name string `go-orm:"PRIMARY KEY"`
+	Age  int
+}
 
 const (
 	server   = "127.0.0.1"
@@ -28,4 +35,48 @@ func OpenDB(t *testing.T) *Engine {
 func TestNewEngine(t *testing.T) {
 	engine := OpenDB(t)
 	defer engine.Close()
+}
+
+func TestEngine_Transaction(t *testing.T) {
+	t.Run("rollback", func(t *testing.T) {
+		transactionRollback(t)
+	})
+	t.Run("commit", func(t *testing.T) {
+		transactionCommit(t)
+	})
+}
+
+// TODO mysql DDL will  automatic commit
+func transactionRollback(t *testing.T) {
+	engine := OpenDB(t)
+	defer engine.Close()
+	s := engine.NewSession()
+	_ = s.Model(&User{}).DropTable()
+	_, err := engine.Transaction(func(s *session.Session) (result interface{}, err error) {
+		_ = s.Model(&User{}).CreateTable()
+		_, err = s.Insert(&User{"Tom", 18})
+		return nil, errors.New("Error")
+	})
+	if err == nil {
+		t.Fatal("failed to rollback")
+	}
+}
+
+func transactionCommit(t *testing.T) {
+	engine := OpenDB(t)
+	defer engine.Close()
+
+	s := engine.NewSession()
+	_ = s.Model(&User{}).DropTable()
+
+	_, err := engine.Transaction(func(s *session.Session) (result interface{}, err error) {
+		_ = s.Model(&User{}).CreateTable()
+		_, err = s.Insert(&User{"Tom", 18})
+		return
+	})
+	u := &User{}
+	_ = s.First(u)
+	if err != nil || u.Name != "Tom" {
+		t.Fatal("failed to commit")
+	}
 }
